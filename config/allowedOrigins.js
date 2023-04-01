0744b1ac824a3cdada53b241af783c4aa1b4184e
midCenturyDude7 <mgriffes@gmail.com>
2023-04-01 11:05:06 -0700

    Minor edit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,10 +1,3 @@
-const allowedOrigins = [
-  "http://127.0.0.1:5500/",
-  "http://localhost:3500/",
-  "http://localhost:3000/",
-  "http://localhost:5173/",
-  "http://localhost:5174/",
-  "http://localhost:5175/",
-];
+const allowedOrigins = ["http://localhost:3500/", "http://localhost:5173/"];
 
 module.exports = allowedOrigins;
