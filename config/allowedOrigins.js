14b223fb081aa3c71ee666b8d84f5795fc5465e2
midCenturyDude7 <mgriffes@gmail.com>
2023-03-31 23:21:54 -0700

    Updated with users api

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
-  "https://localhost:3500",
+  "http://127.0.0.1:5500/",
+  "http://localhost:3500/",
+  "http://localhost:3000/",
   "http://localhost:5173/",
   "http://localhost:5174/",
   "http://localhost:5175/",
